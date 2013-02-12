773d7754f9ff24e33ef4ae0dd76642395fe9ed05
Dennis Boldt <info@dennis-boldt.de>
2013-02-12 14:35:02 +0100

    Improved HashingUtil

--- src/main/java/de/dennis_boldt/utils/HashingUtil.java
+++ src/main/java/de/dennis_boldt/utils/HashingUtil.java
@@ -1,29 +1,63 @@
 package de.dennis_boldt.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import org.apache.commons.io.IOUtils;
+
 public class HashingUtil {
 
-    public static String sha1(String s) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-1");
-            return HexUtil.byteArrayToHexString(md.digest(s.getBytes()));
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public static String sha1(final String s) {
+    	return byteToHash(s.getBytes(), "SHA-1");
     }
 
-    public static String md5(String s) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            return HexUtil.byteArrayToHexString(md.digest(s.getBytes()));
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public static String md5(final String s) {
+    	return byteToHash(s.getBytes(), "MD5");
     }
 
+    public static String sha1(final File f) {
+    	return fileToHash(f, "SHA-1");
+    }
+
+    public static String md5(final File f) {
+    	return fileToHash(f, "MD5");
+    }
+
+    private static String byteToHash(final byte[] bytes, final String algorithm) {
+		try {
+			MessageDigest md = MessageDigest.getInstance(algorithm);
+			return HexUtil.byteArrayToHexString(md.digest(bytes));
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+    }
+
+    private static String fileToHash(final File f, final String algorithm) {
+		try {
+			// TODO: Evaluate/Benchmark: Commons-IO/Guava/JDK-7/
+			//
+			// (1) 	Guava:
+			// 		Files.toByteArray(File file) method
+			// (2) 	JDK-7:
+			// 		Path path = Paths.get("path/to/file");
+			// 		byte[] data = Files.readAllBytes(path);
+			// (3) 	DigestInputStream
+			//		http://www.javablogging.com/sha1-and-md5-checksums-in-java/
+			// (4)	Source
+			//		http://nadeausoftware.com/articles/2008/02/java_tip_how_read_files_quickly
+			return byteToHash(IOUtils.toByteArray(new FileInputStream(f)), algorithm);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
